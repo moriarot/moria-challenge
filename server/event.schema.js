7e3362f7a3f93efe3790d96d57f9d91f94d87266
moriarot <moria.rot@gmail.com>
2023-01-16 13:27:06 +0200

    fix

--- server/event.schema.js
+++ server/event.schema.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 var eventsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-      },
+    name: String,
     startTime: Date,
     endTime: Date,
 });
